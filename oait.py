3bb066fedc6dd3e920bdeed9a8fc73c3a06c8add
Jack Italiano <italiano.16@osu.edu>
2024-02-02 14:37:05 -0500

    Add session threads retrieval and minimum length - pass '-s [session-id]' - pass '-ml [minlen]'

--- oait.py
+++ oait.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import os
-from re import error
+import pdb
 
 import requests
 
@@ -27,7 +27,6 @@ def get_thread_messages(key: str, thread_id: str) -> dict | None:
 
         print("ERROR: " + error_message)
         return None
-
 
     res['thread_id'] = thread_id
 
@@ -69,29 +68,40 @@ def parse_thread_data(thread_data: list) -> list[dict]:
         data_content: list[dict] = data['content']
 
         for content in data_content:
-            if content['type'] == 'text':
-                text: str = content['text']['value']
-                msg: dict[str, str] = { 'role': role, 'text': text }
-                messages.append(msg)
+            text: str = ""
 
-            elif len(content['text']['annotations']) > 0:
+            if content.get('type') == 'text':
+                text: str = content['text']['value']
+
+            elif content.get('type') == 'image_file':
+                file_id: str = content['image_file']['file_id']
+                text: str = f"(Image file) file_id: {file_id}"
+
+            elif len(content.get('text').get('annotations')) > 0:
                 text: str = str(content['text']['annotations'])
-                msg: dict[str, str] = { 'role': role, 'text': text }
-                messages.append(msg)
+
+            msg: dict[str, str] = { 'role': role, 'text': text }
+            messages.append(msg)
 
     return messages
 
 
-def parse_thread(thread_messages: dict) -> dict:
-    thread_data: list[dict] = thread_messages['data']
+def parse_thread(thread_messages: dict, minlen) -> dict:
     thread_id: str = thread_messages['thread_id']
+    try: 
+        thread_data: list[dict] = thread_messages['data']
 
-    parsed_thread: list[dict[str, str]] = parse_thread_data(thread_data)
+        parsed_thread: list[dict[str, str]] = parse_thread_data(thread_data)
 
-    # they're given in reverse order by openai
-    parsed_thread = parsed_thread[::-1]
+        if len(parsed_thread) < minlen:
+            return None
+        # they're given in reverse order by openai
+        parsed_thread = parsed_thread[::-1]
 
-    return {'thread_id': thread_id, 'thread': parsed_thread}
+        return {'thread_id': thread_id, 'thread': parsed_thread}
+    except Exception as e:
+        print(f"ERROR: Parsing thread_id: {thread_id}. error: {e}")
+        return {'thread_id': thread_id, 'thread': 'error'}
 
 
 def parse_session_threads(session_threads: dict) -> list[str]:
@@ -101,14 +111,14 @@ def parse_session_threads(session_threads: dict) -> list[str]:
     return thread_ids
 
 
-def get_threads_from_list(key: str, thread_ids: list[str]):
+def get_threads_from_list(key: str, thread_ids: list[str], minlen: int):
 
     threads: list[dict] = []
     progress: int = 0
     total: int = len(thread_ids)
 
+    update_progress(0, total)
     for thread_id in thread_ids:
-        update_progress(progress, total)
         
         thread = get_thread_messages(key, thread_id)
 
@@ -116,36 +126,41 @@ def get_threads_from_list(key: str, thread_ids: list[str]):
             threads.append(thread)
 
         progress += 1
+        update_progress(progress, total)
 
-    parsed_threads: list[dict] = [parse_thread(thread) for thread in threads]
+    parsed_threads: list[dict] = []
+    for thread in threads:
+        parsed_thread = parse_thread(thread, minlen)
+        if parsed_thread:
+            parsed_threads.append(parsed_thread)
 
     return parsed_threads
 
 
-def get_threads_from_file(key: str, file: str):
+def get_threads_from_file(key: str, file: str, minlen: int):
     file_type: str = os.path.splitext(file)[1][1:]
 
     if file_type == "json":
         thread_ids: list[str] = read_json(file)
-        return get_threads_from_list(key, thread_ids)
+        return get_threads_from_list(key, thread_ids, minlen)
 
     elif file_type == "txt":
         thread_ids: list[str] = read_txt(file)
-        return get_threads_from_list(key, thread_ids)
+        return get_threads_from_list(key, thread_ids, minlen)
     else:
         print(f"ERROR (fatal): Must pass json or txt input file. Got: {file_type}")
         exit(1)
 
 
-def get_threads_from_session_id(key: str, session_id: str, limit=None):
+def get_threads_from_session_id(key: str, session_id: str, minlen: int, limit=None):
     if limit is None:
-        limit = 50
+        limit = 10
 
     sess_threads: dict = get_session_threads(session_id, limit)
     thread_ids: list[str] = parse_session_threads(sess_threads)
 
     if thread_ids:
-        return get_threads_from_list(key, thread_ids)
+        return get_threads_from_list(key, thread_ids, minlen)
     
     else:
         print(f'No threads found for session_id="{session_id}"')
@@ -153,7 +168,7 @@ def get_threads_from_session_id(key: str, session_id: str, limit=None):
 
 
 def update_progress(progress, total):
-    print(f"\rProgress:[{progress}/{total}]", end="") 
+    print(f"Progress:[{progress}/{total}]", end="\r") 
 
 def read_txt(file_path: str):
     with open(file_path, 'r') as file:
@@ -244,6 +259,7 @@ def run(args: argparse.Namespace):
     output: str = args.output
     session_id:str = args.session
     limit:str = args.limit
+    minlen: int = args.minlen
 
     if thread_ids and file:
         print("ERROR (fatal): Must pass thread_ids as a list of cli args OR a file input. Not both.")
@@ -251,13 +267,13 @@ def run(args: argparse.Namespace):
 
     parsed_threads: list[dict] = []
     if thread_ids:
-        parsed_threads = get_threads_from_list(key, thread_ids)
+        parsed_threads = get_threads_from_list(key, thread_ids, minlen)
 
     elif file:
-        parsed_threads = get_threads_from_file(key, file)
+        parsed_threads = get_threads_from_file(key, file, minlen)
 
     elif session_id:
-        parsed_threads = get_threads_from_session_id(key, session_id, limit)
+        parsed_threads = get_threads_from_session_id(key, session_id, minlen, limit)
 
     else:
         print("ERROR (fatal): Must pass list of thread_ids (space separated), an input file (json list or newline separated txt), or session.")
@@ -279,6 +295,7 @@ def main():
     parser.add_argument('--output', '-o', nargs='?', type=str, help="Output file (json). (Pass only -o for default: output.json)", const="output.txt", default=None)
     parser.add_argument('--session', '-s', type=str, help="Get thread messages from session id (Navigate to https://platform.openai.com/assistants. Copy Authorization header 'sess-')")
     parser.add_argument('--limit', '-l', type=int, help="Limit for number of session threads")
+    parser.add_argument('--minlen', '-ml', type=int, help="Minimum length of threads to include in output.", default=1)
 
     args = parser.parse_args()
     
